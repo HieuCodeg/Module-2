741664b0c4ddf95d9e08ab9371a5c242189355c4
HieuCodeg <108601145+HieuCodeg@users.noreply.github.com>
2022-08-05 16:10:54 +0700

    bai tap 1

---
+++ 1. Introduction to Java/system-time/src/SystemTime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
